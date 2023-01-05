0400bfed6da1bb45a9242c29599029063a23fb11
VasaOfficial <VasaPofficial@gmail.com>
2023-01-05 15:03:45 +0100

    Add form for user education info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ContactInfo from "./components/ContactInfo";
+import EducationalInfo from "./components/Education";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <ContactInfo />
+        <EducationalInfo />
       </div>
     );
   }
---
+++ src/components/Education.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+class EducationalInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      school: "",
+      study: "",
+      date: "",
+    };
+  }
+
+  // updating stored value
+  handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  //  prevents defoult action
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          School:
+          <input
+            type="text"
+            name="school"
+            value={this.state.school}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Title of Study:
+          <input
+            type="text"
+            name="study"
+            value={this.state.study}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Date of Study:
+          <input
+            type="date"
+            name="date"
+            value={this.state.date}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+        <br />
+        <p>School: {this.state.school}</p>
+        <p>Title of Study: {this.state.study}</p>
+        <p>Date of Study: {this.state.date}</p>
+      </form>
+    );
+  }
+}
+
+export default EducationalInfo;
