39c911a00fdad6d5c0b37eb22ee9e4a3d5e97a55
VasaOfficial <VasaPofficial@gmail.com>
2023-01-05 21:27:19 +0100

    Add card like format and header

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -22,43 +22,53 @@ class ContactInfo extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Phone:
-          <input
-            type="tel"
-            name="phone"
-            value={this.state.phone}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-        <br />
-        <p>Name: {this.state.name}</p>
-        <p>Email: {this.state.email}</p>
-        <p>Phone: {this.state.phone}</p>
-      </form>
+      <>
+        <h1 className="header">CV Generator</h1>
+        <form className="generalInfo" onSubmit={this.handleSubmit}>
+          <div className="basicInput">
+            <label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                required
+                value={this.state.name}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <br />
+            <label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+                value={this.state.email}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <br />
+            <label>
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Phone"
+                required
+                value={this.state.phone}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+            <br />
+          </div>
+          <div className="basicInfo">
+            <p>Name: {this.state.name}</p>
+            <p>Email: {this.state.email}</p>
+            <p>Phone: {this.state.phone}</p>
+          </div>
+        </form>
+      </>
     );
   }
 }
--- src/components/Education.js
+++ src/components/Education.js
@@ -23,42 +23,46 @@ class EducationalInfo extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          School:
-          <input
-            type="text"
-            name="school"
-            value={this.state.school}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Title of Study:
-          <input
-            type="text"
-            name="study"
-            value={this.state.study}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Date of Study:
-          <input
-            type="date"
-            name="date"
-            value={this.state.date}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-        <br />
-        <p>School: {this.state.school}</p>
-        <p>Title of Study: {this.state.study}</p>
-        <p>Date of Study: {this.state.date}</p>
+      <form className="generalInfo" onSubmit={this.handleSubmit}>
+        <div className="basicInfo">
+          <label>
+            <input
+              type="text"
+              name="school"
+              placeholder="School"
+              value={this.state.school}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="study"
+              placeholder="Title of Study"
+              value={this.state.study}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="date"
+              name="date"
+              placeholder="Date of Study"
+              value={this.state.date}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+          <br />
+        </div>
+        <div className="basicInfo">
+          <p>School: {this.state.school}</p>
+          <p>Title of Study: {this.state.study}</p>
+          <p>Date of Study: {this.state.date}</p>
+        </div>
       </form>
     );
   }
